18556ce886c16f0ee9329e8ef8b485149330e8e3
jp-the-horiuchi <r.horiuchi.engineer.com>
2024-10-10 14:01:16 +0900

    add:タスク作成画面追加

---
+++ src/app/memo/create.tsx
@@ -0,0 +1,40 @@
+import { View, StyleSheet, TextInput, KeyboardAvoidingView } from 'react-native';
+import CircleButton from '../../components/CircleButton';
+import { Feather } from '@expo/vector-icons';
+import { router } from 'expo-router';
+
+const Create = (): JSX.Element => {
+    const handlePress = (): void => {
+        router.back();
+    };
+    return (
+        <KeyboardAvoidingView behavior="height" style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput multiline style={styles.input} value="" />
+            </View>
+            <CircleButton onPress={handlePress}>
+                <Feather name="check" size={40} color="ffffff" />
+            </CircleButton>
+        </KeyboardAvoidingView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff'
+    },
+    inputContainer: {
+        paddingVertical: 32,
+        paddingHorizontal: 27,
+        flex: 1
+    },
+    input: {
+        flex: 1,
+        textAlignVertical: 'top',
+        fontSize: 16,
+        lineHeight: 24
+    }
+});
+
+export default Create;
