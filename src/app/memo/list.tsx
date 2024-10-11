037ca2704efb68c3b00ee331de746a6d32332aab
jp-the-horiuchi <r.horiuchi.engineer.com>
2024-10-11 17:24:25 +0900

    add:メモ一覧をDBから受け取る

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,16 +1,20 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import MemoListItem from '../../components/MemoListItem';
 import CircleButton from '../../components/CircleButton';
 import { Feather } from '@expo/vector-icons';
 import { router, useNavigation } from 'expo-router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import LogOutButton from '../../components/LogOutButton';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { auth, db } from '../../config';
+import { Memo } from '../../types/memo';
 
 const handlePress = (): void => {
     router.push('/memo/create');
 };
 
 const List = (): JSX.Element => {
+    const [memos, setMemos] = useState<Memo[]>([]);
     const navigation = useNavigation();
     useEffect(() => {
         navigation.setOptions({
@@ -20,13 +24,33 @@ const List = (): JSX.Element => {
         });
     }, []);
 
+    useEffect(() => {
+        if (auth.currentUser === null) return;
+        const ref = collection(db, `users/${auth.currentUser.uid}/memos`);
+        const q = query(ref, orderBy('updatedAt', 'desc'));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            const remoteMemos: Memo[] = [];
+            snapshot.forEach((doc) => {
+                const { bodyText, updatedAt } = doc.data();
+                remoteMemos.push({
+                    id: doc.id,
+                    bodyText,
+                    updatedAt
+                });
+            });
+            setMemos(remoteMemos);
+        });
+        return unsubscribe;
+    }, []);
+
     return (
         <View style={styles.container}>
-            <View>
-                <MemoListItem />
-                <MemoListItem />
-                <MemoListItem />
-            </View>
+            <FlatList
+                data={memos}
+                renderItem={({ item }) => {
+                    return <MemoListItem memo={item} />;
+                }}
+            />
             <CircleButton onPress={handlePress}>
                 <Feather name="plus" size={40} />
             </CircleButton>
