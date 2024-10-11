a1a0dd1a53681732a0cbe37779bee102db71051f
jp-the-horiuchi <r.horiuchi.engineer.com>
2024-10-11 17:24:07 +0900

    add:テキスト編集処理追加

--- src/app/memo/edit.tsx
+++ src/app/memo/edit.tsx
@@ -1,18 +1,60 @@
-import { View, StyleSheet, TextInput, KeyboardAvoidingView } from 'react-native';
+import { View, StyleSheet, TextInput, Alert } from 'react-native';
 import CircleButton from '../../components/CircleButton';
 import { Feather } from '@expo/vector-icons';
-import { router } from 'expo-router';
+import { router, useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { auth, db } from '../../config';
+import { Timestamp, doc, getDoc, setDoc } from 'firebase/firestore';
+import KeyboardAvoidingView from '../../components/KeyboardAvoidingView';
+
+const handlePress = (id: string, bodyText: string): void => {
+    if (auth.currentUser === null) return;
+    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id);
+    setDoc(ref, { bodyText, updatedAt: Timestamp.fromDate(new Date()) })
+        .then(() => {
+            router.back();
+        })
+        .catch((error) => {
+            console.log(error);
+            Alert.alert('更新に失敗しました');
+        });
+};
 
 const Edit = (): JSX.Element => {
-    const handlePress = (): void => {
-        router.back();
-    };
+    const id = String(useLocalSearchParams().id);
+    const [bodyText, setBodyText] = useState<string>('');
+
+    useEffect(() => {
+        if (auth.currentUser === null) return;
+        const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id);
+        getDoc(ref)
+            .then((docRef) => {
+                const RemoteBodyText = docRef?.data()?.bodyText;
+                setBodyText(RemoteBodyText);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
     return (
-        <KeyboardAvoidingView behavior="height" style={styles.container}>
+        <KeyboardAvoidingView style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput multiline style={styles.input} value="買い物リスト" />
+                <TextInput
+                    onChangeText={(text) => {
+                        setBodyText(text);
+                    }}
+                    multiline
+                    style={styles.input}
+                    value={bodyText}
+                    autoFocus
+                />
             </View>
-            <CircleButton onPress={handlePress}>
+            <CircleButton
+                onPress={() => {
+                    handlePress(id, bodyText);
+                }}
+            >
                 <Feather name="check" size={40} color="ffffff" />
             </CircleButton>
         </KeyboardAvoidingView>
@@ -25,15 +67,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#ffffff'
     },
     inputContainer: {
-        paddingVertical: 32,
-        paddingHorizontal: 27,
         flex: 1
     },
     input: {
         flex: 1,
         textAlignVertical: 'top',
         fontSize: 16,
-        lineHeight: 24
+        lineHeight: 24,
+        paddingVertical: 32,
+        paddingHorizontal: 27
     }
 });
 
